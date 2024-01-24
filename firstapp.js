8d0eefec0ba67a409f56bc101930f6749f0ae246
Shivam Bhardwaj <64952948+hazard-web@users.noreply.github.com>
2024-01-24 19:34:19 +0530

    Return Responses

--- firstapp.js
+++ firstapp.js
@@ -2,17 +2,33 @@
 const http = require('http');
 
 function serverListener(req , res) {
-    console.log(req);
-    res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end('Shivam Bhardwaj\n');
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    // res.end('Shivam Bhardwaj\n');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+
+    if (req.url === '/home'){
+        res.write('<body><h1>Welcome home</h1></body>');
+    }else if (req.url === '/about'){
+        res.write('<body><h1>Welcome to About Us page</h1></body>'); 
+    }else if (req.url === '/node'){
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    }else{
+        res.write('<body><h1>Page not found</h1></body>');
+    }
+    res.write('</html>');
+    res.end();
+    
 }
 
 const server = http.createServer(serverListener);
 
-const PORT = 8000;   //PORT 4000 is already in use thats why i have taken 8000:
+const PORT = 3000;   //PORT 4000 is already in use thats why i have taken 8000:
 
 server.listen(PORT, () => {
-    console.log("Server is running at port 8000");
+    console.log("Server is running at port 3000");
 });
 
 server.on('error', (error) => {
