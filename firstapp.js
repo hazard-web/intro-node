c1b86248d27cb303b27bb57601828e3b145d865b
Shivam Bhardwaj <64952948+hazard-web@users.noreply.github.com>
2024-01-25 01:35:21 +0530

    Display  the message on the server(which store in message.txt fie) readFile and writeFile  both async function used for non blocking operations of events, http and fs module used.

--- firstapp.js
+++ firstapp.js
@@ -1,40 +1,69 @@
-
 const http = require('http');
+const fs = require('fs');
 
-function serverListener(req , res) {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    // res.end('Shivam Bhardwaj\n');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+function serverListener(req, res) {
 
-    if (req.url === '/home'){
-        res.write('<body><h1>Welcome home</h1></body>');
-    }else if (req.url === '/about'){
-        res.write('<body><h1>Welcome to About Us page</h1></body>'); 
-    }else if (req.url === '/node'){
-        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
-    }else{
-        res.write('<body><h1>Page not found</h1></body>');
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        fs.readFile('message.txt', (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            console.log(`data from file` + data);
+            res.write('<html>');
+            res.write(`<head><title>Enter Message</title></head>`);
+            res.write(`<body><h1>${data}</h1></body>`);
+            res.write(`<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>`);
+            res.write('</html>');
+            return res.end();
+        });
+    } else if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                if (err) {
+                    console.log('Error writing a file:', err);
+                    res.statusCode = 500;
+                    return res.end();
+                }
+                console.log(`inside fs.writeFile`);
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    } else {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write(`<head><title>My first page</title></head>`);
+        res.write(`<body>Hello from node.js</body>`);
+        res.write('</html>');
+        res.end();
     }
-    res.write('</html>');
-    res.end();
-    
 }
 
 const server = http.createServer(serverListener);
 
-const PORT = 3000;   //PORT 4000 is already in use thats why i have taken 8000:
+const PORT = 3000;
 
 server.listen(PORT, () => {
     console.log("Server is running at port 3000");
 });
 
 server.on('error', (error) => {
-    if(error.code === 'EADDRINUSE'){
+    if (error.code === 'EADDRINUSE') {
         console.log(`Port ${PORT} is already in use`);
-    }else{
-        console.log(`An error occured: ${error.message}`);
+    } else {
+        console.log(`An error occurred: ${error.message}`);
     }
-})
+});
