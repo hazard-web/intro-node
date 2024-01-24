989838262972eab3462a58cdf54b07007b8627a5
Shivam Bhardwaj <64952948+hazard-web@users.noreply.github.com>
2024-01-24 18:46:38 +0530

    Create Server (with if and else conditions if server is in use return as server alsresy in use , else return the error with error meesage)

--- firstapp.js
+++ firstapp.js
@@ -1,5 +1,24 @@
-console.log('Hello World From Node.js');
 
-const fs = require('fs');
+const http = require('http');
 
-fs.writeFileSync('hello.txt', 'Hello World');
+function serverListener(req , res) {
+    console.log(req);
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.end('Shivam Bhardwaj\n');
+}
+
+const server = http.createServer(serverListener);
+
+const PORT = 8000;   //PORT 4000 is already in use thats why i have taken 8000:
+
+server.listen(PORT, () => {
+    console.log("Server is running at port 8000");
+});
+
+server.on('error', (error) => {
+    if(error.code === 'EADDRINUSE'){
+        console.log(`Port ${PORT} is already in use`);
+    }else{
+        console.log(`An error occured: ${error.message}`);
+    }
+})
