202805763079a5231e02c3d6a6df7837c2aa409f
Shivam Bhardwaj <64952948+hazard-web@users.noreply.github.com>
2024-01-27 20:51:01 +0530

    Create add-product console it using body-parser(User clicks on submit parse both the input tags and show it on console)

--- firstapp.js
+++ firstapp.js
@@ -1,34 +1,41 @@
-const http = require('http');
-const express = require('express');
 
-// const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next();
-})
+app.use(bodyParser.urlencoded({extented : false}));
 
-app.use((req, res, next) => {
-    console.log('In the another middleware');
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`<form action="/product" method="POST">
+        <div>
+        <input type="text" name="Title"/>
+        <input type="number" name='Quantity'/>
+        </div>
+        <button type="submit">Add Product</button>
+    </form>`);
 });
 
-const server = http.createServer(app);
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
-
-
+app.use('/', (req, res, next) => {
+    res.send(`<h1>Hello From Express!</h1>`);
+});
 
 const PORT = 3000;
 
-server.listen(PORT, () => {
-    console.log("Server is running at port 3000");
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
 });
 
-server.on('error', (error) => {
-    if (error.code === 'EADDRINUSE') {
-        console.log(`Port ${PORT} is already in use`);
-    } else {
-        console.log(`An error occurred: ${error.message}`);
+app.on('error', (error) =>{
+    if(error.code === 'EADDRINUSE'){
+        console.log(`A port ${PORT} is already in use`);
+    }else{
+        console.log(`Error: ${error.message}`);
     }
 });
